4875e91f327cf38baffd8193b9ffbc170a8f4267
luthfi1 <68719137+luthfi1@users.noreply.github.com>
2021-01-04 09:35:37 +0700

    Add files via upload

---
+++ VPN/Shared/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by muhammad Luthfi farizqi on 01/01/21.
+//  Copyright © 2021 muhammad Luthfi farizqi . All rights reserved.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+
+        Home()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VPN/Shared/VPNApp.swift
@@ -0,0 +1,18 @@
+//
+//  VPNApp.swift
+//  Shared
+//
+//  Created by muhammad Luthfi farizqi on 01/01/21.
+//  Copyright © 2021 muhammad Luthfi farizqi . All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct VPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
